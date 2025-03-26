9beb4d9d28a3c930645e1613e43b077d2f06cd57
JoaoPedroVidal9 <jp.joaopedro.franc0@gmail.com>
2025-03-26 16:24:30 -0300

    Correção da senha login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,14 +5,18 @@ import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import { Link, useNavigate } from "react-router-dom";
+import IconButton from "@mui/material/IconButton";
 import { useState } from "react";
-import api from "../axios/axios"
-import senai from '../assets/logo_senai.png'
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import api from "../axios/axios";
+import senai from "../assets/logo_senai.png";
 
 function Login() {
   const [user, setUser] = useState({
     cpf: "",
     password: "",
+    showPassword: false,
   });
 
   const navigate = useNavigate();
@@ -27,57 +31,69 @@ function Login() {
     login();
   };
 
+  function isOn() {
+    if (user.showPassword) {
+      return "text";
+    } else {
+      return "password";
+    }
+  }
+
   async function login() {
     await api.postLogin(user).then(
       (response) => {
-        alert(response.data.message)
-        localStorage.setItem('authenticated', true);
-        navigate('salas/')
+        alert(response.data.message);
+        localStorage.setItem("authenticated", true);
+        navigate("salas/");
       },
       (error) => {
-        console.log(error)
-        alert(error.response.data.error)
+        console.log(error);
+        alert(error.response.data.error);
       }
-    )
+    );
   }
 
-
   return (
-    <Container component="main" maxWidth="xl" style={{ display: 'flex', padding: "0", margin: "0" }} >
+    <Container
+      component="main"
+      maxWidth="xl"
+      style={{ display: "flex", padding: "0", margin: "0" }}
+    >
       <Box
         sx={{
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
           justifyContent: "space-evenly",
-          padding: "3%"
+          padding: "3%",
         }}
       >
-
         <img
           style={{
             width: "300px",
-            height: "75px"
+            height: "75px",
           }}
-          src={senai} />
+          src={senai}
+        />
 
         <Typography
-          sx={{ marginTop: "40px", fontSize: '26px', }}
+          sx={{ marginTop: "40px", fontSize: "26px" }}
           component="h1"
-          textAlign="center">
-          Seja bem-vindo(a). Faça o login para acessar a Agenda Senai ou cadastre-se como novo usuário.
+          textAlign="center"
+        >
+          Seja bem-vindo(a). Faça o login para acessar a Agenda Senai ou
+          cadastre-se como novo usuário.
         </Typography>
 
         <Box
           component="form"
           onSubmit={handleSubmit}
           noValidate
-          sx={{ width: "100%", display:"flex", flexDirection:"column" }}
+          sx={{ width: "100%", display: "flex", flexDirection: "column" }}
         >
-
-          <Typography
-            sx={{ marginTop: "15px", fontSize:"20px" }}
-          >Usuário</Typography>
+          <Typography sx={{ marginTop: "15px", fontSize: "20px" }}>
+            Usuário
+          </Typography>
           <TextField
             required
             fullWidth
@@ -94,58 +110,86 @@ function Login() {
               },
             }}
             sx={{
-              paddingBottom: "10px", marginTop: "15px", border: "1px solid black", borderRadius: '15px',
-              '.MuiInputLabel-root': {
-                paddingLeft: "5px"
-              }
-            }}  
-          />
-
-          <Typography
-            sx={{ marginTop: "15px", fontSize:"20px" }}
-          >Senha</Typography>
-          <TextField
-            required
-            fullWidth
-            margin="normal"
-            label="Digite sua senha"
-            name="password"
-            id="password"
-            value={user.password}
-            onChange={onChange}
-            variant="standard"
-            slotProps={{
-              input: {
-                disableUnderline: true,
+              paddingBottom: "10px",
+              marginTop: "15px",
+              border: "1px solid black",
+              borderRadius: "15px",
+              ".MuiInputLabel-root": {
+                paddingLeft: "5px",
               },
             }}
-            sx={{
-              paddingBottom: "10px", marginTop: "15px", border: "1px solid black", borderRadius: '15px',
-              '.MuiInputLabel-root': {
-                paddingLeft: "5px"
-              }
-            }}  
           />
+
+          <Typography sx={{ marginTop: "15px", fontSize: "20px" }}>
+            Senha
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              border: "1px solid black",
+
+              borderRadius: "15px",
+            }}
+          >
+            <TextField
+              required
+              fullWidth
+              margin="normal"
+              label="Digite sua senha"
+              name="password"
+              id="password"
+              type={isOn()}
+              value={user.password}
+              onChange={onChange}
+              variant="standard"
+              slotProps={{
+                input: {
+                  disableUnderline: true,
+                },
+              }}
+            />
+            <IconButton
+              sx={{width:"10px", height:"10px", paddingTop:"10px",paddingRight:"20px"}}
+              onClick={() =>
+                setUser({ ...user, showPassword: !user.showPassword })
+              }
+            >
+              {user.showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+            </IconButton>
+          </Box>
+
           <Button
             type="submit"
             fullWidth
             variant="contained"
-            sx={{ width:"90%", marginTop: "15px", backgroundColor: "#215299", alignSelf:"center" }}
+            sx={{
+              width: "90%",
+              marginTop: "15px",
+              backgroundColor: "#215299",
+              alignSelf: "center",
+            }}
           >
             Entrar
           </Button>
 
-          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <p style={{ margin: '5px' }}>Não possui conta? </p>
-            <Link
-              to='/cadastro'
-              sx={{ margin: '5px', marginTop: "15px" }}
-            > Cadastre-se </Link>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <p style={{ margin: "5px" }}>Não possui conta? </p>
+            <Link to="/cadastro" sx={{ margin: "5px", marginTop: "15px" }}>
+              {" "}
+              Cadastre-se{" "}
+            </Link>
           </div>
-
         </Box>
       </Box>
-
     </Container>
   );
 }
